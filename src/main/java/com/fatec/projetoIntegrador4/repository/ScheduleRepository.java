abdf63080de47b4e5a31c48fef601e55d72d8556
Gabriel Ruiz <gabrielruiztq@gmail.com>
2020-11-04 06:39:26 -0300

    Remove project

--- src/main/java/com/fatec/projetoIntegrador4/repository/ScheduleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fatec.projetoIntegrador4.repository;
-
-import com.fatec.projetoIntegrador4.models.Schedule;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScheduleRepository extends JpaRepository<Schedule, Long>{
-    
-}
--- src/main/java/com/fatec/projetoIntegrador4/repository/TeamRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fatec.projetoIntegrador4.repository;
-
-import com.fatec.projetoIntegrador4.models.Team;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeamRepository extends JpaRepository<Team, Long>{
-    
-}
