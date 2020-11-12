84156d254d0e090bc8403f39a986f1aab5ea221d
Gabriel Ruiz <gabrielruiztq@gmail.com>
2020-11-12 10:00:16 -0300

    Exercício 3
    Adicionado uma funcionalidade que permite definir no url um id de cliente, para listar todos os veículos que ele possui

--- projetoIntegrador4/src/main/java/com/fatec/projetoIntegrador4/controllers/EquipeController.java
+++ projetoIntegrador4/src/main/java/com/fatec/projetoIntegrador4/controllers/EquipeController.java
@@ -1,14 +1,31 @@
 package com.fatec.projetoIntegrador4.controllers;
 
+import java.util.List;
+
+import com.fatec.projetoIntegrador4.models.Equipe;
+import com.fatec.projetoIntegrador4.services.EquipeService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class EquipeController {
     
+    // Service
+    @Autowired
+    private EquipeService equipeService;
+
+
+
+    // Index
     @GetMapping("/equipe")
-    public String equipe() 
+    public String equipe(Model model) 
     {
+        List<Equipe> equipes = equipeService.findAll();
+        model.addAttribute("equipes", equipes);
+
         return "pages/equipe/equipe";
     }
 }
