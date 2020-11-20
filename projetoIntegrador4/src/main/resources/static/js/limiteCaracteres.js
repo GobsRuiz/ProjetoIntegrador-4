f9da45fbfc7bf876fa5b2dc6fdc023173cf19a76
Gabriel Ruiz <gabrielruiztq@gmail.com>
2020-11-20 09:00:16 -0300

    Alteração no limite de caracteres

--- projetoIntegrador4/src/main/resources/static/js/limiteCaracteres.js
+++ projetoIntegrador4/src/main/resources/static/js/limiteCaracteres.js
@@ -2,10 +2,10 @@
 var noticias_texto = document.querySelectorAll(".noticia-slider-card-texto");
 
 noticias_texto.forEach(element => {
-    if(screen.width > 1200){
+    if(screen.width > 1024){
         element.innerHTML = element.innerHTML.substr(0, 120) + "...";
     }
-    else if(screen.width < 1200){
+    else if(screen.width < 1024){
         element.innerHTML = element.innerHTML.substr(0, 80) + "...";
     }
 });
@@ -16,10 +16,12 @@ noticias_texto.forEach(element => {
 var dashNoticias_texto = document.querySelectorAll(".dashNoticiaTexto");
 
 dashNoticias_texto.forEach(element => {
-    if(screen.width > 1200){
-        element.innerHTML = element.innerHTML.substr(0, 500) + "...";
+    if(screen.width > 1024){
+        if(element.innerHTML.length > 200){
+            element.innerHTML = element.innerHTML.substr(0, 400) + "...";
+        }
     }
-    else if(screen.width < 1200){
+    else if(screen.width < 1024){
         element.innerHTML = element.innerHTML.substr(0, 80) + "...";
     }
 });
